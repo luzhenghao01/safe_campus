60fabe040435f2ff9a297d00ab7ebaf07ee69bcb
luzhenghao <luzhenghao@bodypark.ai>
2022-03-11 09:42:25 +0800

    init

---
+++ electricFence/src/main/java/com/lzh/electricfence/ElectricFenceApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.electricfence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricFenceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElectricFenceApplication.class, args);
+  }
+
+}
